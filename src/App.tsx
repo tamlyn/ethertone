3c1a939f190306bdf7b91ef5e2a29ea5d59150d2
Tamlyn Rhodes <tamlyn@tamlyn.org>
2023-09-26 20:25:41 +0100

    Add Elementary Audio

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { el } from '@elemaudio/core'
+import WebRenderer from '@elemaudio/web-renderer'
+import { useEffect } from 'react'
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const ctx = new AudioContext()
+const core = new WebRenderer()
+
+core.on('load', function () {
+  core.render(el.cycle(440), el.cycle(441))
+})
+
+function App() {
+  useEffect(() => {
+    core.initialize(ctx).then((node) => node.connect(ctx.destination))
+  }, [])
+
+  return <h1>Ethertone</h1>
 }
 
 export default App
